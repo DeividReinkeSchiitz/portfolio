dd540da7b07531bd7d11346d86f4daf365dff0b5
Deivid R. S. <deivid@schiitz.com>
2026-03-20 19:35:45 -0400

    fix tailwind

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   //set output directory
   distDir: "build",
+  images: {
+    qualities: [60, 75, 85, 90, 95],
+  },
 };
 
 export default nextConfig;
